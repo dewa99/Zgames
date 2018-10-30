8ee581f640cdee1a439030aa2deeeb26bf6f02c8
dewa99 <dodeardi@gmail.com>
2018-10-30 10:17:44 +0700

    nice

--- scripts/c_switch_image.gd
+++ scripts/c_switch_image.gd
@@ -2,10 +2,7 @@ extends Node2D
 
 var a = []
 var anim = []
-<<<<<<< HEAD
 var b = []
-=======
->>>>>>> 521f49c72363f92f16872414d05e282da4445097
 var index = 0
 var qs 
 var as 
@@ -14,22 +11,16 @@ var combo_pressed = 0
 var get_index = []
 var timer
 var timer_2
-<<<<<<< HEAD
 var finish_state
 var level_finish_indicator = 0
-=======
->>>>>>> 521f49c72363f92f16872414d05e282da4445097
 
 func _ready():
 	a.resize(6)
 	anim.resize(6)
 	get_index.resize(6)
-<<<<<<< HEAD
 	b.resize(6)
 	
 	
-=======
->>>>>>> 521f49c72363f92f16872414d05e282da4445097
 	
 	as = null
 	qs = null
@@ -40,7 +31,6 @@ func _ready():
 	a[3]= get_node("../kotak4")
 	a[4]= get_node("../kotak5")
 	a[5]= get_node("../kotak6")
-<<<<<<< HEAD
 	
 	anim[0] = get_node("../kotak1/AnimationPlayer")
 	anim[1] = get_node("../kotak2/AnimationPlayer")
@@ -62,18 +52,6 @@ func _ready():
 	timer_2 = get_node("../turn_on")
 	
 	finish_state.hide()
-=======
-	
-	anim[0] = get_node("../kotak1/AnimationPlayer")
-	anim[1] = get_node("../kotak2/AnimationPlayer")
-	anim[2] = get_node("../kotak3/AnimationPlayer")
-	anim[3] = get_node("../kotak4/AnimationPlayer")
-	anim[4] = get_node("../kotak5/AnimationPlayer")
-	anim[5] = get_node("../kotak6/AnimationPlayer")
-	
-	timer = get_node("../turn_off")
-	timer_2 = get_node("../turn_on")
->>>>>>> 521f49c72363f92f16872414d05e282da4445097
 	
 	set_process(true)
 	pass
@@ -136,12 +114,8 @@ func change_tex(index):
 	yield(anim[index],"finished")
 	anim[index].play("open")
 	a[index].set_normal_texture(load("res://UI/button_green.png"))
-<<<<<<< HEAD
 	b[index].show()
 	
-=======
-	yield(anim[index],"finished")
->>>>>>> 521f49c72363f92f16872414d05e282da4445097
 	
 func cek_jawaban(a,b,c):
 	if a == b and c==2:
@@ -150,32 +124,21 @@ func cek_jawaban(a,b,c):
 		combo_pressed=0
 		as = rand_range(1,10)
 		qs = rand_range(1,10)
-<<<<<<< HEAD
 		level_finish_indicator += 1
 	
 	
-=======
->>>>>>> 521f49c72363f92f16872414d05e282da4445097
 	
 	if a!=b and c==2:
 		print("false")
 		timer.start()
 		jawaban_salah()
 		combo_pressed=0
-<<<<<<< HEAD
 		as = rand_range(1,14)
 		qs = rand_range(1,14)
 	
 func image_default(index):
 	a[index].set_normal_texture(load("res://UI/back_button.png"))
 	b[index].hide()
-=======
-		as = rand_range(1,10)
-		qs = rand_range(1,10)
-	
-func image_default(index):
-	a[index].set_normal_texture(load("res://UI/back_button.png"))
->>>>>>> 521f49c72363f92f16872414d05e282da4445097
 
 func jawaban_benar():
 	for i in range(0,6,1):
@@ -189,14 +152,11 @@ func jawaban_salah():
 		if get_index[i]==1:
 			get_index[i]=0
 
-<<<<<<< HEAD
 func level_finished():
 	if level_finish_indicator == 3:
 		finish_state.show()
 		get_tree().set_pause(true)
 
-=======
->>>>>>> 521f49c72363f92f16872414d05e282da4445097
 func _on_turn_off_timeout():
 	for i in range(0,6,1):
 		image_default(i)
@@ -209,10 +169,7 @@ func _on_turn_on_timeout():
 		if get_index[i]==1:
 			a[i].hide()
 	jawaban_benar()
-<<<<<<< HEAD
 	level_finished()
-=======
->>>>>>> 521f49c72363f92f16872414d05e282da4445097
 	pass # replace with function body
 
 
